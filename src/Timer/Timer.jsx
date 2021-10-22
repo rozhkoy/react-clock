a5c460e91dca680917adda2e12f47df15ad21992
zolir <rc.drift18@gmail.com>
2021-10-22 23:10:32 +0200

    add new changes

--- src/Timer/Timer.jsx
+++ src/Timer/Timer.jsx
@@ -1,16 +1,18 @@
 import react from "react";
+import { useState } from "react";
 import TimerSectionItem from "./TimerSectionItem";
-
+// const [StateTimer , setStateTimer] = useState(true)
+const StateTimer = true;
 const Timer = () => {
     
     return(
         <div class="wrap-timer">
                 <div class="timer__section-group">
-                    <TimerSectionItem desription={'Hrs'} maxSize={24}/>
+                    <TimerSectionItem desription={'Hrs'} maxSize={24} stateTimer={StateTimer} />
                     <span class="timer-colon">:</span>
-                    <TimerSectionItem desription={'Min'} maxSize={60} />
+                    <TimerSectionItem desription={'Min'} maxSize={60} stateTimer={StateTimer}/>
                     <span class="timer-colon">:</span>
-                    <TimerSectionItem desription={'Sec'} maxSize={60} />
+                    <TimerSectionItem desription={'Sec'} maxSize={60} stateTimer={StateTimer}/>
                 </div>
                 <div class="timer-button__group">
                     <button class="timer__button timer__button--start">Start</button>
--- src/Timer/TimerSectionItem.jsx
+++ src/Timer/TimerSectionItem.jsx
@@ -1,34 +1,55 @@
-import react, { useState }  from "react";
+import react, { useEffect, useState }  from "react";
 import arrow from "./arrow.svg"
 
 const TimerSectionItem = (props) => {
-    const [indicator, SetIndicator] = useState(0);
-    
+    const [indicator, SetIndicator] = useState(16);
+    let timer ;
+
+    useEffect(() =>{
+        timer = setInterval(() => {
+        SetIndicator((indicator) => indicator - 1)
+        }, 500);
+        if(indicator <= 0){
+            
+            SetIndicator(60)
+        }
+        return () => {
+            clearInterval(timer);
+        }
+    })
 
     function minus(){
-        if(indicator <= 0){
-            SetIndicator(props.maxSize);
+        if(!props.stateTimer){
+            if(indicator <= 0){
+                SetIndicator(props.maxSize);
 
+            }else{
+                SetIndicator((indicator) => indicator - 1);
+            }
         }else{
-            SetIndicator((indicator) => indicator - 1);
-        }
-        }
-    function plus(){
-        if(indicator == props.maxSize){
-            SetIndicator(0);
-
-        }else{
-            SetIndicator((indicator) => indicator + 1);
+            console.log("timer now worked")
         }
     }
-        return(
+    function plus(){
+        if(!props.stateTimer){
+            if(indicator == props.maxSize){
+                SetIndicator(0);
 
-        <div class="timer__section-item">
-            <button class=" timer__button button__plus" onClick={plus}><img src={arrow} alt=""/></button>
-            <span class="numer__item">{indicator}</span>
-            <span class="number__desription">{props.desription}</span>
-            <button class=" timer__button button__minus" onClick={minus}><img src={arrow} alt=""/></button>
-        </div>
+            }else{
+                SetIndicator((indicator) => indicator + 1);
+            }
+        }else{
+            console.log("timer now worked")
+        }    
+    }
+    return(
+
+    <div class="timer__section-item">
+        <button class=" timer__button button__plus" onClick={plus}><img src={arrow} alt=""/></button>
+        <span class="numer__item">{indicator < 10 ? "0" + indicator: indicator}</span>
+        <span class="number__desription">{props.desription}</span>
+        <button class=" timer__button button__minus" onClick={minus}><img src={arrow} alt=""/></button>
+    </div>
     )
 }
 export default TimerSectionItem;
