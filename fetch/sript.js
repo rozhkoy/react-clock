e5487fe7c67fd99337347b69e15b28d1194eefc9
zolir18 <rc.drift18@gmail.com>
2021-11-01 11:54:04 +0100

    add new changes

--- fetch/sript.js
+++ fetch/sript.js
@@ -3,6 +3,7 @@
     let array = []
 
  function go(){
+    console.time();
    fetch('https://restcountries.com/v2/all')
       .then(response => response.json())
       .then(commits => {
@@ -13,6 +14,8 @@
                array[i].name = commits[i].name;
                if('capital' in commits[i]){
                   array[i].capital = commits[i].capital;
+               }else{
+                  array[i].capital = commits[i].name;
                }
                array[i].latlng = commits[i].latlng;
             
@@ -22,14 +25,37 @@
       })
       .then(() =>{
          console.log(array);
-         for(let j = 0; j < 10; j++ ){
-            fetch(`https://api.ipgeolocation.io/timezone?apiKey=1951161faacc41268be75b771f166a97&lat=${array[j].latlng[0]}&long=${array[j].latlng[1]}`)
-               .then(response => response.json())
-               .then(commits => console.log(commits))
-         }
+         
+         // for(let j = 0; j < 10; j++ ){
+         //    fetch(`https://api.ipgeolocation.io/timezone?apiKey=1951161faacc41268be75b771f166a97&lat=${array[j].latlng[0]}&long=${array[j].latlng[1]}`)
+         //       .then(response => response.json())
+         //       .then(commits => console.log(this.commits))
+         // }
       })
+   console.timeEnd()
 
    
  }
+   
+
+
+ function search(word){
+    console.time()
+   let arraySimilarWord = []
+   regex = new RegExp((`^${word}`), 'i','m');
+
+   for(let k = 0; k < array.length; k++){
+      if(array[k].capital.match(regex)){
+         console.log(array[k].capital.match(regex));
+          arraySimilarWord.push(array[k].capital)
+      }
+   }
+   console.log(arraySimilarWord);
+   console.timeEnd();
+ }
+
+
  go()
+
+
  
