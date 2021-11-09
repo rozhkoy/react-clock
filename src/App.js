d2ef57b0bde2d1d1ac52ce3b72e59c377c2ef3d6
zolir18 <rc.drift18@gmail.com>
2021-11-09 18:37:50 +0100

    add new saerch

--- src/App.js
+++ src/App.js
@@ -2,15 +2,43 @@
 import './App.css';
 import Wrap from './Wrap/Wrap';
 import TabsButton from './TabsButton/TabsButton';
-import { useRef } from 'react';
+import { createContext, useEffect, useRef, useState } from 'react';
+
+export const countryListObject = createContext([])
 
 function App() {
+  const countryList = useRef([])    
+  const [stateApi , setStateApi] = useState(false);
+  useEffect(() =>{
+      if(stateApi == false){
+          fetch('https://restcountries.com/v2/all')
+              .then(response => response.json())
+              .then(commits => {
+                  for(let i = 0; i < commits.length; i++){
+                  countryList.current[i] = {}
+                      
+                  countryList.current[i].index = i;
+                  countryList.current[i].name = commits[i].name;
+                      if('capital' in commits[i]){
+                          countryList.current[i].capital = commits[i].capital;
+                      }else{
+                          countryList.current[i].capital = commits[i].name;
+                      }
+                      countryList.current[i].region = commits[i].region;
 
+                  }
+                  console.log(countryList.current);
+              })
+              setStateApi(true)
+      }
+  })
   return (
     <div className="App">
+      <countryListObject.Provider value={countryList.current}>
        <Wrap>
           <TabsButton/>
        </Wrap>
+      </countryListObject.Provider>
     </div>
   );
 }
--- src/Clock/Clock.jsx
+++ src/Clock/Clock.jsx
@@ -1,62 +1,40 @@
-import react, { useEffect, useRef, useState } from "react";
-import ClockSectionItem from "./ClocksectionItem";
-import { DateTime, Settings } from "luxon";
-
+import react, { useContext, useEffect, useRef, useState } from 'react';
+import ClockSectionItem from './ClocksectionItem';
+import { DateTime, Settings } from 'luxon';
+import { countryListObject } from '../App';
 
 const Clock = () => {
-    let dateTime = DateTime.local();
-    const resultListArray = useRef([])
+    const ListForHints = useContext(countryListObject);
+    const resultListArray = useRef([]);
     const counterRow = useRef(-1);
-    const [stateApi , setStateApi] = useState(false);
-    const searchDate = useRef({enteredText: 'jakoma'})
+    const searchDate = useRef({ enteredText: 'jakoma' });
+    const [showHints, setShowHints] = useState(false);
     const [enteredText, setEnteredText] = useState('');
+    const searchInput = useRef('null');
+    const hitsList = useRef(null);
     const [rusultList, setResultList] = useState([
-        {id: 0, text: "Moscow, Russia"},
-        {id: 1, text: "Tokyo, Japan"},
-        {id: 2, text: "Oslo, Norway"},
-        {id: 3, text: "Tokyo, Japan"}])
-    const countryList = useRef([])    
-    
-    useEffect(() =>{
-        if(stateApi == false){
-            fetch('https://restcountries.com/v2/all')
-                .then(response => response.json())
-                .then(commits => {
-                    for(let i = 0; i < commits.length; i++){
-                    countryList.current[i] = {}
-                        
-                    countryList.current[i].index = i;
-                    countryList.current[i].name = commits[i].name;
-                        if('capital' in commits[i]){
-                            countryList.current[i].capital = commits[i].capital;
-                        }else{
-                            countryList.current[i].capital = commits[i].name;
-                        }
-                        countryList.current[i].region = commits[i].region;
-
-                    }
-                    console.log(countryList.current);
-                })
-                setStateApi(true)
-        }
-    })
-
+        { id: 0, text: 'Moscow, Russia' },
+        { id: 1, text: 'Tokyo, Japan' },
+        { id: 2, text: 'Oslo, Norway' },
+        { id: 3, text: 'Tokyo, Japan' },
+    ]);
 
     function UpdateInput(event) {
         let temporally = event.target.value;
         console.log(temporally);
         searchDate.current.enteredText = temporally;
         setEnteredText(temporally);
+        createHintsList();
     }
-    
-    function output(index, last) {
-        console.log("entered",index,  rusultList[index])
+
+    function changeInputDate(index, last) {
+        console.log('entered', index, rusultList[index]);
         counterRow.current = index;
-        if(last == true && index == -1){
+        if (last == true && index == -1) {
             setEnteredText(searchDate.current.enteredText);
-        }else if(last == true && index == rusultList.length){
+        } else if (last == true && index == rusultList.length) {
             setEnteredText(searchDate.current.enteredText);
-        }else{
+        } else {
             let offerResult = rusultList[index].text;
             setEnteredText(offerResult);
         }
@@ -64,144 +42,130 @@ const Clock = () => {
 
     function changeResult(event) {
         //  to bottom
-        if(event.keyCode === 40){
-            resultListArray.current[rusultList.length - 1].classList.remove('active__list')
+        if (event.keyCode === 40) {
+            resultListArray.current[rusultList.length - 1].classList.remove('active__list');
             counterRow.current++;
-            if(counterRow.current >= rusultList.length){
+            if (counterRow.current >= rusultList.length) {
                 console.log(counterRow.current);
                 counterRow.current = -1;
-                console.log("sweg")
-                output(counterRow.current, true)
+                console.log('sweg');
+                changeInputDate(counterRow.current, true);
             }
-            if(resultListArray.current[counterRow.current]){
+            if (resultListArray.current[counterRow.current]) {
                 resultListArray.current[counterRow.current].classList.add('active__list');
                 console.log(counterRow.current);
-                output(counterRow.current)
-                if(resultListArray.current[counterRow.current].previousSibling){
+                changeInputDate(counterRow.current);
+                if (resultListArray.current[counterRow.current].previousSibling) {
                     resultListArray.current[counterRow.current].previousSibling.classList.remove('active__list');
-                }   
-            } 
+                }
+            }
         }
         // to top
-        if(event.keyCode === 38){
+        if (event.keyCode === 38) {
             counterRow.current--;
-            if(counterRow.current <  -1){
+            if (counterRow.current < -1) {
                 counterRow.current = rusultList.length - 1;
                 resultListArray.current[0].classList.remove('active__list');
             }
-            if(counterRow.current == -1){
-                console.log("sweg")
-                output(counterRow.current, true)
+            if (counterRow.current == -1) {
+                console.log('sweg');
+                changeInputDate(counterRow.current, true);
                 counterRow.current = rusultList.length;
-                resultListArray.current[0].classList.remove('active__list');        
-            }    
-            if(resultListArray.current[counterRow.current]){
+                resultListArray.current[0].classList.remove('active__list');
+            }
+            if (resultListArray.current[counterRow.current]) {
                 resultListArray.current[counterRow.current].classList.add('active__list');
                 console.log(counterRow.current);
-                output(counterRow.current)
-                if(resultListArray.current[counterRow.current + 1]){
+                changeInputDate(counterRow.current);
+                if (resultListArray.current[counterRow.current + 1]) {
                     resultListArray.current[counterRow.current + 1].classList.remove('active__list');
-                } 
-            }            
+                }
+            }
         }
     }
-    
 
-    return(
+    function createHintsList() {
+        console.log('createHintsList');
+        let regex = new RegExp(`^${enteredText}`, 'i', 'm');
+        let newID = 0;
+        let newResultList = [{ id: 0, text: 'huj' }];
+        for (let i = 0; i < ListForHints.length; i++) {
+            // console.log(ListForHints[i].capital.match(regex));
+            if (ListForHints[i].capital.match(regex)) {
+                console.log(ListForHints[i].capital.match(regex));
+                newResultList = rusultList.splice();
+                newID++;
+                newResultList.push({ id: newID, text: ListForHints[i].capital });
+            } else {
+            }
+        }
+        setResultList(newResultList);
+    }
+
+    function focusInpit() {
+        hitsList.current.classList.add('hintsList');
+    }
+
+    return (
         <div class="wrap-clock">
-                <div className="search">
-                    <input type="text" class="search__input" placeholder="Search by country or capital" onKeyDown={changeResult}  value={enteredText} onChange={UpdateInput}/>
-                    <button class="search__bttn">Search</button>
-                    <ul className="search__result" >
-                        {rusultList.map((Item)=> <li ref={elRef => resultListArray.current.push(elRef)} onClick={() =>output(Item.id, false)} key={Item.id}>{Item.text}</li> )}
-                    </ul>
-
-                </div>
-                <div class="clock__section-group">
-                <ClockSectionItem  description={"Hrs"}/>
+            <div className="search" onFocus={focusInpit}>
+                <input type="text" class="search__input" placeholder="Search by country or capital" onKeyDown={changeResult} value={enteredText} onChange={UpdateInput} />
+                <button class="search__bttn">Search</button>
+                <ul className="search__result" ref={hitsList}>
+                    {rusultList.map((Item) => (
+                        <li ref={(elRef) => resultListArray.current.push(elRef)} onClick={() => changeInputDate(Item.id, false)} key={Item.id}>
+                            {Item.text}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div class="clock__section-group">
+                <ClockSectionItem description={'Hrs'} />
                 <span class="clock-colon">:</span>
-                <ClockSectionItem  description={"Min"}/>
+                <ClockSectionItem description={'Min'} />
                 <span class="clock-colon">:</span>
-                <ClockSectionItem  description={"Sec"}/>
+                <ClockSectionItem description={'Sec'} />
+            </div>
+            <p class="clock__date">Sunday, November 07</p>
+            <div class="preview-time">
+                <div class="preview-time__item">
+                    <div class="preview-time__capital">London</div>
+                    <div class="preview-time__sub-timer">14:34</div>
                 </div>
-                <p class="clock__date">Sunday, November 07</p>
-                <div class="preview-time">
-                    <div class="preview-time__item">
-                        <div class="preview-time__capital">
-                            London
-                        </div>
-                        <div class="preview-time__sub-timer">
-                            14:34
-                        </div>
-                    </div>
-                    <div class="preview-time__item">
-                        <div class="preview-time__capital">
-                            los-hujantos
-                        </div>
-                        <div class="preview-time__sub-timer">
-                            14:34
-                        </div>
-                    </div>
-                    <div class="preview-time__item">
-                        <div class="preview-time__capital">
-                            Kyiv
-                        </div>
-                        <div class="preview-time__sub-timer">
-                            14:34
-                        </div>
-                    </div>
-                    <div class="preview-time__item">
-                        <div class="preview-time__capital">
-                            London
-                        </div>
-                        <div class="preview-time__sub-timer">
-                            14:34
-                        </div>
-                    </div>
-                    <div class="preview-time__item">
-                        <div class="preview-time__capital">
-                            Moscow
-                        </div>
-                        <div class="preview-time__sub-timer">
-                            14:34
-                        </div>
-                    </div>
-                    <div class="preview-time__item">
-                        <div class="preview-time__capital">
-                            Paris
-                        </div>
-                        <div class="preview-time__sub-timer">
-                            14:34
-                        </div>
-                    </div>
-                    <div class="preview-time__item">
-                        <div class="preview-time__capital">
-                            Paris
-                        </div>
-                        <div class="preview-time__sub-timer">
-                            14:34
-                        </div>
-                    </div>
-                    <div class="preview-time__item">
-                        <div class="preview-time__capital">
-                            Tokyo
-                        </div>
-                        <div class="preview-time__sub-timer">
-                            14:34
-                        </div>
-                    </div>
-                    <button onclick="alert('time')" class="preview-time__item">
-                        <div class="preview-time__capital">
-                            Kigalikili
-                        </div>
-                        <div class="preview-time__sub-timer">
-                            14:34
-                        </div>
-                    </button>
-
+                <div class="preview-time__item">
+                    <div class="preview-time__capital">los-hujantos</div>
+                    <div class="preview-time__sub-timer">14:34</div>
                 </div>
-            
+                <div class="preview-time__item">
+                    <div class="preview-time__capital">Kyiv</div>
+                    <div class="preview-time__sub-timer">14:34</div>
+                </div>
+                <div class="preview-time__item">
+                    <div class="preview-time__capital">London</div>
+                    <div class="preview-time__sub-timer">14:34</div>
+                </div>
+                <div class="preview-time__item">
+                    <div class="preview-time__capital">Moscow</div>
+                    <div class="preview-time__sub-timer">14:34</div>
+                </div>
+                <div class="preview-time__item">
+                    <div class="preview-time__capital">Paris</div>
+                    <div class="preview-time__sub-timer">14:34</div>
+                </div>
+                <div class="preview-time__item">
+                    <div class="preview-time__capital">Paris</div>
+                    <div class="preview-time__sub-timer">14:34</div>
+                </div>
+                <div class="preview-time__item">
+                    <div class="preview-time__capital">Tokyo</div>
+                    <div class="preview-time__sub-timer">14:34</div>
+                </div>
+                <button onclick="alert('time')" class="preview-time__item">
+                    <div class="preview-time__capital">Kigalikili</div>
+                    <div class="preview-time__sub-timer">14:34</div>
+                </button>
+            </div>
         </div>
-    )
-}
+    );
+};
 export default Clock;
